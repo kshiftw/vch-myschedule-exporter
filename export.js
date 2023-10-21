79b3e53f91aa94d0d800ddb45412ec3342a659fc
Kirk Wong <kirk.wong@clio.com>
2023-10-21 15:52:09 -0700

    Add extension

---
+++ export.js
@@ -0,0 +1,444 @@
+// Helper function for adding delay
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+/*
+ *   Converts date string to iCal format.
+ *   Example:
+ *       Input: Sep 01, 2020
+ *       Output: 20200901
+ */
+function convertDateToICal(date) {
+    let day = moment(date, "MMM DD YYYY");
+    let iCalString = day.format("YYYYMMDD");
+
+    return iCalString;
+}
+
+/*
+ *    Converts duration string to iCal format and the start/end time.
+ *    Example:
+ *       Input: 07:00 - 15:00 PDT
+ *       Output: [070000, 150000, 07:00, 15:00]
+ */
+function convertEntryTimeToICal(duration) {
+    let durationString = duration.replace(/ (PDT|PST)$/, "");
+    let startTimeString = durationString.split(" - ")[0];
+    let endTimeString = durationString.split(" - ")[1];
+
+    let startTime = moment(startTimeString, "HH:mm");
+    let endTime = moment(endTimeString, "HH:mm");
+
+    let iCalStartTime = startTime.format("HHmmss");
+    let iCalEndTime = endTime.format("HHmmss");
+
+    return [iCalStartTime, iCalEndTime, startTimeString, endTimeString];
+}
+
+/* 
+ *    Trims whitespace from status
+ */
+function cleanStatus(status) {
+    let cleanStatus = status.trim()
+    return cleanStatus;
+}
+
+/*
+ *   Removes \n and all excess spaces in between words.
+ */
+function cleanUnit(unit) {
+    let cleanUnit = unit.replace(/\n\s*/g, ' ');
+    return cleanUnit;
+}
+
+/*
+ *    Generates a random iCal UID.
+ *    Example: 
+ *        Output: 2c9f6b5b-5b4b-4e2e-9c3c-4f5a7a4a5f5a
+ */
+function generateICalUID() {
+    let uid = '';
+    const hexChars = '0123456789abcdef';
+    for (let i = 0; i < 32; i++) {
+        if (i === 8 || i === 12 || i === 16 || i === 20) {
+            uid += '-';
+        }
+        uid += hexChars.charAt(Math.floor(Math.random() * hexChars.length));
+    }
+    return uid;
+}
+
+/*
+ * Returns the summary of an entry based on the settings.
+ */
+function getSummaryFromSettings(entry, settings) {
+    let summary;
+
+    let titleSetting = settings.title;
+    let customTitleSetting = settings.custom_title;
+
+    if (titleSetting === 'custom') {
+        summary = customTitleSetting;
+    }
+    else {
+        summary = entry[titleSetting]
+    }
+
+    return summary;
+}
+
+/*
+ *  Converts an entry object to iCal format.
+ */
+function convertEntryToICal(entry, settings) {
+    const timezone = 'America/Vancouver';
+
+    const { name, union, unit, icon, date, dateText, startTime, endTime, startTimeString, endTimeString, duration, paycode, status } = entry;
+
+    let summary = getSummaryFromSettings(entry, settings);
+
+    let iCalContent =
+        'BEGIN:VEVENT\n' +
+        'UID:' + generateICalUID() + '\n' +
+        'DTSTAMP:' + moment().format("YYYYMMDDTHHmmss") + '\n' +
+        'DTSTART;TZID=' + `${timezone}:${date}T${startTime}` + '\n' +
+        'DTEND;TZID=' + `${timezone}:${date}T${endTime}` + '\n' +
+        'LOCATION:' + unit + '\n' +
+        'SUMMARY:' + summary + '\n' +
+        'DESCRIPTION:' +
+        'Shift: ' + name + '\\n' +
+        'Date: ' + dateText + '\\n' +
+        'Time: ' + duration + '\\n' +
+        'Type: ' + status + '\\n' +
+        'Location: ' + unit + '\\n' +
+        'Union: ' + union + '\\n' +
+        'Pay Code: ' + paycode + '\\n\n' +
+        'END:VEVENT\n';
+
+    // Remove double spaces from content.
+    iCalContent = iCalContent.replace(/\s{2,}/g, ' ');
+
+    return iCalContent;
+}
+
+/*
+ *   Wraps iCal content with iCal header and footer.
+ */
+function wrapICalContent(iCalContent) {
+    return 'BEGIN:VCALENDAR\n' +
+        'VERSION:2.0\n' +
+        'PRODID:-//VCH MySchedule Exporter//EN\n' +
+        iCalContent +
+        'END:VCALENDAR\n';
+}
+
+/*
+ *  Parses a row in the schedule table and returns an entry object.
+ */
+function getEntryFromRow(row) {
+    let columns = $(row).children();
+
+    let name = columns.eq(0).text();
+    let union = columns.eq(1).text();
+    let unit = columns.eq(2).text();
+    let icon = columns.eq(3).text();
+    let dateText = columns.eq(4).text();
+    let duration = columns.eq(5).text();
+    let paycode = columns.eq(6).text();
+    let status = columns.eq(7).find('span').text();
+
+    let date = convertDateToICal(dateText);
+    let [startTime, endTime, startTimeString, endTimeString] = convertEntryTimeToICal(duration);
+
+    status = cleanStatus(status);
+    unit = cleanUnit(unit);
+
+    return {
+        name: name,
+        union: union,
+        unit: unit,
+        icon: icon,
+        date: date,
+        dateText: dateText,
+        startTime: startTime,
+        endTime: endTime,
+        start_time: startTimeString,
+        end_time: endTimeString,
+        duration: duration,
+        paycode: paycode,
+        status: status,
+    }
+}
+
+/*
+ *   Redirects to a specific page in the My Shifts page.
+ */
+function goToPage(pageNumber) {
+    let url = window.location.href;
+
+    if (url.includes('page=')) {
+        newUrl = url.replace(/(page=)\d+/, `page=${pageNumber}`);
+    } else {
+        newUrl = url.concat(`&page=${pageNumber}`);
+    }
+
+    window.location.replace(newUrl);
+}
+
+/*
+ *  Returns an array of entries in the current page.
+ */
+function getEntriesPerPage() {
+    let entriesInPage = [];
+
+    let table = $('table#schedule-table');
+    let tableRows = table.find('tbody').children();
+
+    tableRows.each(function (_, row) {
+        let entry = getEntryFromRow(row);
+
+        entriesInPage.push(entry);
+    });
+
+    return entriesInPage;
+}
+
+/*
+ *  Returns the number of pages in the My Shifts page.
+ */
+function getNumPages() {
+    let numPages = $('nav.pagination').find('ul.pagination-list').children().length;
+    return numPages;
+}
+
+/*
+ *   Generates a downloadable iCal file using the given iCal content.
+ */
+function downloadICalFile(iCalContentArray, fileName) {
+    let iCalContent = wrapICalContent(iCalContentArray.join(''));
+
+    let blob = new Blob([iCalContent], { type: 'text/calendar;charset=UTF-8' });
+    let url = URL.createObjectURL(blob);
+    let link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    link.click();
+
+    // Clean up the URL object
+    URL.revokeObjectURL(url);
+}
+
+/*
+ *  Converts an array of entries to an array of iCal content.
+ */
+async function convertEntriesToICal(entries) {
+    let iCalContentArray = [];
+
+    let settings;
+    await chrome.runtime.sendMessage({ action: 'getSettings' }, async (response) => {
+        settings = response;
+    });
+    await delay(1000);
+    await entries.forEach(async entry => {
+        if (typeof entry === 'undefined') {
+            return;
+        }
+
+        if (entry.status === 'Working' && !settings.include_working_status) {
+            return;
+        }
+
+        if (entry.status === 'Planned Leave' && !settings.include_planned_leave_status) {
+            return;
+        }
+
+        let iCalContent = convertEntryToICal(entry, settings);
+        iCalContentArray.push(iCalContent);
+    });
+
+    return iCalContentArray;
+}
+
+/*
+ *   Returns the array of entries from the background script.
+ */
+async function getEntries() {
+    let entries;
+    await chrome.runtime.sendMessage({ action: 'getStatus' }, async (response) => {
+        entries = response.entries;
+    });
+    await delay(1000);
+    return entries;
+}
+
+/*
+ * Extracts course schedule info and creates a downloadable iCalendar (.ics) file.
+ */
+let exportCalendar = async function () {
+    console.log("Exporting Calendar to ics file...")
+
+    let entries = await getEntries();
+
+    if (entries.length === 0) {
+        alert('Unable to create a schedule. No shifts were found for the selected year. Please make sure the selected year is correct.');
+        return false;
+    } else {
+        let iCalContentArray = await convertEntriesToICal(entries);
+        let fileName = 'vch-my-schedule.ics';
+        downloadICalFile(iCalContentArray, fileName);
+    }
+};
+
+/*
+ * Appends the Export Calendar section to the My Shifts page.
+ */
+function appendExportDiv() {
+    $('div.box > form').first().find('div.form-actions').append(
+        '<hr /> \
+        <h3 class="title is-3">Export Calendar</h3> \
+        <div class="control-group field"> \
+            <p>Export the search results to an iCalendar (.ics) file. This file can be imported into Google Calendar, Microsoft Outlook, and other calendar applications.</p> \
+        </div> \
+        <div class="control-group field"> \
+            <label class="control-label label">Calendar Event Title</label> \
+            <div class="control form-field-width select"> \
+                <select name="selectTitle" id="selectTitle" class="select"> \
+                    <option value="name">Occ.</option> \
+                    <option value="unit">Unit</option> \
+                    <option value="duration">Duration</option> \
+                    <option value="start_time">Start Time</option> \
+                    <option value="end_time">End Time</option> \
+                    <option value="status">Status</option> \
+                    <option value="custom">Custom Text</option> \
+                </select> \
+            </div> \
+        </div> \
+        <div id="customTitleDiv" class="control-group field" style="display: none;"> \
+            <label class="control-label label">Custom Event Title</label> \
+            <div class="control form-field-width"> \
+                <input type="text" id="customTitle" name="customTitle" class="input" placeholder="Enter custom title"> \
+            </div> \
+        </div> \
+        <div id="selectStatuses" class="control-group field"> \
+            <label class="control-label label">Status</label> \
+            <div class="controls"> \
+                <ul style="margin-left: 0; margin-top: 0"> \
+                    <li>\
+                        <label class="checkbox-label" for="id_checkbox_working">\
+                            <input checked name="checkbox_working" id="id_checkbox_working" type="checkbox" class="checkbox-input" style="margin-right: 5px;">\
+                         Working</label> \
+                    </li> \
+                    <li>\
+                        <label class="checkbox-label" for="id_checkbox_planned_leave">\
+                            <input checked name="checkbox_planned_leave" id="id_checkbox_planned_leave" type="checkbox" class="checkbox-input" style="margin-right: 5px;">\
+                         Planned Leave</label> \
+                    </li> \
+                </ul> \
+            </div> \
+        </div> \
+        <input id="export" value="Download file 🚀" class="btn btn-primary button is-primary submit-button-top-margin">'
+    )
+}
+
+/*
+ * Updates the status in the background script.
+ */
+function updateStatus(numPages, currentPage, entriesInPage) {
+    let newStatus = {
+        numPages: numPages,
+        currentPage: currentPage,
+        entriesInPage: entriesInPage
+    }
+
+    chrome.runtime.sendMessage({ action: 'updateStatus', data: newStatus });
+}
+
+/*
+ * Updates the status in the background script to indicate that export has started.
+ */
+function exportStarted() {
+    chrome.runtime.sendMessage({ action: 'exportStarted' });
+    updateSettings();
+}
+
+/*
+ * Resets the status in the background script.
+ */
+function exportReset() {
+    chrome.runtime.sendMessage({ action: 'exportReset' });
+}
+
+/*
+ * Updates the settings in the background script.
+ */
+function updateSettings() {
+    let titleSetting = $('#selectTitle').val();
+    let customTitleSetting = $('#customTitle').val();
+
+    let includeWorkingStatus = $('#id_checkbox_working').prop('checked');
+    let includePlannedLeaveStatus = $('#id_checkbox_planned_leave').prop('checked');
+
+    let newSettings = {
+        title: titleSetting,
+        custom_title: customTitleSetting,
+        include_working_status: includeWorkingStatus,
+        include_planned_leave_status: includePlannedLeaveStatus
+    }
+
+    chrome.runtime.sendMessage({ action: 'updateSettings', data: newSettings });
+}
+
+$(document).ready(function () {
+    // Show dropdown only if on the "My Shifts" page
+    if (window.location.href.includes('myschedule.vch.ca/employee/sched/readonly/employee')) {
+        appendExportDiv();
+    };
+
+    // Show/hide custom title input if selected option is "custom"
+    $('#selectTitle').change(function () {
+        let selectedOption = $(this).val()
+        let customTitleDiv = document.getElementById('customTitleDiv');
+
+        if (selectedOption === 'custom') {
+            customTitleDiv.style.display = 'inline';
+        } else {
+            customTitleDiv.style.display = 'none';
+        }
+    });
+
+    // Reset export status when the "Search" button is clicked
+    $('#submit-id-submit').click(function () {
+        exportReset();
+    })
+
+    chrome.runtime.sendMessage({ action: 'getStatus' }, async (response) => {
+        const { numPages, currentPage, exportStarted } = response;
+
+        if (exportStarted) {
+            if (currentPage <= numPages) {
+                let nextPage = currentPage + 1
+
+                let entriesInPage = getEntriesPerPage();
+                await delay(1000);
+
+                updateStatus(numPages, nextPage, entriesInPage);
+                goToPage(nextPage);
+            }
+            else {
+                exportCalendar();
+            }
+        }
+    });
+
+    // Export calendar when the "Download file" button is clicked
+    $(document).on('click', '#export', async function (event) {
+        console.log("Export initiated...")
+
+        let numPages = getNumPages();
+        let currentPage = 1;
+        let entriesInPage = [];
+
+        exportStarted();
+        updateStatus(numPages, currentPage, entriesInPage);
+        goToPage(currentPage);
+    });
+});
